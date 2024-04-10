9eaf9731da3e5209ea421bf11dbbd56350934e45
Mircea Popa <pmircea027@gmail.com>
2024-04-10 18:55:07 +0300

    Add height style to .App and loading animation

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import {
   TableView,
   TableHeader,
@@ -7,12 +6,13 @@ import {
   TableBody,
   Row,
   Cell,
+  ProgressCircle,
 } from "@adobe/react-spectrum";
-import { parseDate } from "@internationalized/date";
 
 function Brands(props) {
   const [date, setDate] = useState(props.date);
   const [brandData, setBrandData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false); // State to manage loading indicator
 
   const columns = [
     { name: "Brand Name", uid: "name" },
@@ -26,37 +26,54 @@ function Brands(props) {
     const month = date.month;
     const year = date.year;
     return new Date(year, month - 1, day).getTime();
-  }
+  };
 
   useEffect(() => {
+    setIsLoading(true);
     setDate(props.date);
-    fetch("http://localhost:5000/brands?start=" + convertDate(props.date.start) + "&end=" + convertDate(props.date.end))
+    fetch(
+      "http://localhost:5000/brands?start=" +
+        convertDate(props.date.start) +
+        "&end=" +
+        convertDate(props.date.end)
+    )
       .then((response) => response.json())
       .then((data) => {
         setBrandData(data);
-        console.log("Data fetched: ", data);
-      }).catch((error) => {
+        setIsLoading(false);
+      })
+      .catch((error) => {
         console.error("Error fetching data: ", error);
+        setIsLoading(false);
       });
   }, [props.date]);
 
   return (
     <div>
-      <TableView aria-label="Example table with dynamic content">
-        <TableHeader columns={columns}>
-          {(column) => <Column key={column.uid}>{column.name}</Column>}
-        </TableHeader>
-        <TableBody items={brandData}>
-          {(item) => (
-            <Row key={item.brandname}>
-              <Cell>{item.brandname}</Cell>
-              <Cell>{item.profiles.length}</Cell>
-              <Cell>{item.followers}</Cell>
-              <Cell>{item.engagement}</Cell>
-            </Row>
-          )}
-        </TableBody>
-      </TableView>
+      {isLoading ? (
+        <div
+          className="loader"
+          style={{ textAlign: "center", paddingTop: "2rem" }}
+        >
+          <ProgressCircle aria-label="Loading…" isIndeterminate size="L" />
+        </div>
+      ) : (
+        <TableView aria-label="Example table with dynamic content">
+          <TableHeader columns={columns}>
+            {(column) => <Column key={column.uid}>{column.name}</Column>}
+          </TableHeader>
+          <TableBody items={brandData}>
+            {(item) => (
+              <Row key={item.brandname}>
+                <Cell>{item.brandname}</Cell>
+                <Cell>{item.profiles.length}</Cell>
+                <Cell>{item.followers}</Cell>
+                <Cell>{item.engagement}</Cell>
+              </Row>
+            )}
+          </TableBody>
+        </TableView>
+      )}
     </div>
   );
 }
